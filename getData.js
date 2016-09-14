0d23ff499c279558cef6fc97543c208ea4ecf106
TetsuyaNegishi <tetsuya.negishi.tsukuba@gmail.com>
2016-09-14 21:32:33 +0900

    [update]隣接を用いて探索するようにリファクタリング

--- getData.js
+++ getData.js
@@ -3,25 +3,28 @@ const client = require('cheerio-httpcli');
 const kinejunURL = 'https://ja.wikipedia.org/wiki/%E3%82%AD%E3%83%8D%E3%83%9E%E6%97%AC%E5%A0%B1';
 
 client.fetch(kinejunURL, function(err, $, res) {
-    $('h4 > span.mw-headline').each(function(idx) {
+	$('h4').each(function(idx) {
+		console.log($(this).get(0).tagName);
         console.log($(this).text());
+		let tag = $(this);
+		while (	tag = tag.next()) {
+			if(tag.get(0).tagName === 'ul') {
+				continue;
+			}
+			if(tag.get(0).tagName === 'p'){
+				console.log('break');
+				break;
+			}
+			const janle = tag.find('p b').text();
+			console.log(janle);
+			let rank = 1;
+			$('li', 'ol', tag).each(function(idx) {
+				let title = $(this).text();
+				if (title !== '-') {
+					console.log(rank + ':' + $(this).text());
+				}
+				rank++;
+			});
+		}
     });
-    $('div#mw-content-text > div').each(function(idx) {
-        //console.log($(this p b).text());
-        const t = $(this).find('p b').text();
-        if (t !== '') {
-            console.log(t);
-            var rank = $(this).find('ol li');
-            var i = 1;
-            $('li', 'ol', this).each(function(idx) {
-                var n = $(this).text();
-                if (n !== '-') {
-                    console.log(i + ':' + $(this).text());
-                }
-                i++
-            });
-            // console.log(rank);
-        }
-    })
-
 });
